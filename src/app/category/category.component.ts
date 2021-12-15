74f6be7d0b9ce73be7020ab84c4d32188d54056f
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-16 00:44:33 +0300

    refactor: category.component revised.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  title = 'Kategori Listesi';
+  categories: Category[] = [
+    { id: 1, name: 'Elektronik' },
+    { id: 2, name: 'Bilgisayar' },
+    { id: 3, name: 'Telefon' },
+    { id: 4, name: 'Müzik' },
+    { id: 5, name: 'Giyim' },
+    { id: 6, name: 'İçecek' },
+  ];
+  ngOnInit(): void {}
 }
