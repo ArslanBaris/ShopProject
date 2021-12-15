76aeef761f083bea17f9471f78b6174f43a9ca95
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-15 17:58:54 +0300

    feat: category component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
