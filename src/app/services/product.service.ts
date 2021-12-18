d232c2524ebfe83668f15abc914e95f46b7cac99
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-19 00:17:01 +0300

    Observable ile Loglama

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import {tap,catchError} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,19 @@ export class ProductService {
   path = "http://localhost:3000/products";
   
   getProducts() : Observable<Product[]>{
-    return this.http.get<Product[]>(this.path);
+    return this.http.get<Product[]>(this.path).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.hadleError)
+    );
+  }
+  hadleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if(err.error instanceof ErrorEvent){
+      errorMessage = ' Bir hata oluştu ' + err.error.message;
+    }else{
+      errorMessage = "Sistemsel bir hata";
+    }
+    return throwError(errorMessage);
+    
   }
 }
